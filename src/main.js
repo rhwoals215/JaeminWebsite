dff7bd742337fcd59e553964ed9561371990d569
rhwoals215@gmail.com <rhwoals215@gmail.com>
2020-04-16 10:31:43 -0700

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 import vuetify from './plugins/vuetify';
 
+Vue.use(Vuex)
+
 Vue.config.productionTip = false
+
 
 new Vue({
   vuetify,
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#4F7EEA',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#CB4039',
+    success: '#64BE79',
+    warning: '#EDCA74',
+    'green-accent': '#24ccb8',
+  },
+});
 
 export default new Vuetify({
-});
+  theme: {
+    dark: true,
+  },
+})
